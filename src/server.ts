8143c98767b9cf0f993ff80d76a599aa5dea2bff
sincerajn <sincerajn@outlook.com>
2026-04-05 09:30:54 +0800

    chore: bump version to 0.13.0

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import {
 } from './tools'
 
 const name = 'rimsage'
-const version = '0.12.1'
+const version = '0.13.0'
 const sandbox = new PathSandbox('dist/assets')
 
 function registerToolsAndResources(server: McpServer) {
